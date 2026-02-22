e4de97ad9db97886c558cf9789a6631fa835614f
Matthew Love <matthewlove94@gmail.com>
2026-02-22 18:19:27 +0000

    Make class to hold drawing fns

--- sandbox_img_gen/cli/main.cpp
+++ sandbox_img_gen/cli/main.cpp
@@ -9,87 +9,84 @@
 namespace mgk = Magick;
 
 namespace sbx {
-static auto blank_image(std::size_t width, std::size_t height) {
-    constexpr std::int64_t offset{0};
+class ImgGenerator {
+  public:
+    ImgGenerator() = delete;
+    ImgGenerator(std::size_t width, std::size_t height)
+        : width_{width}
+        , height_{height}
+        , circle_drawer_{static_cast<double>(width_), static_cast<double>(height_)} {}
+    auto blank_image() const {
+        constexpr std::int64_t offset{0};
 
-    mgk::Geometry const geometry{width, height, offset, offset};
-    mgk::ColorRGB const colour{1.f, 1.f, 1.f, 1.f};
-    mgk::Image image{geometry, colour};
+        mgk::Geometry const geometry{width_, height_, offset, offset};
+        mgk::ColorRGB const colour{1.f, 1.f, 1.f, 1.f};
+        mgk::Image image{geometry, colour};
 
-    return image;
-}
-
-static void draw_circle() {
-    constexpr std::size_t width{1024uz};
-    constexpr std::size_t height{width};
-    auto image{blank_image(width, height)};
-
-    sbx::CircleDrawer drawer{width, height};
-    auto const circle{drawer.draw_centre(0.1)};
-    image.draw(circle);
-    image.write("circle.png");
-}
-static void draw_grid() {
-    constexpr std::size_t width{1024uz};
-    constexpr std::size_t height{width};
-    auto image{blank_image(width, height)};
-
-    sbx::CircleDrawer drawer{width, height};
-
-    auto const circles{drawer.draw_centred_rect_grid(2u, 2u, 0.05)};
-    for (auto const& c : circles) {
-        image.draw(c);
+        return image;
     }
+    void draw_circle() {
+        auto image{blank_image()};
 
-    image.write("grid_image_0.png");
-}
-
-static void
-    draw_rect_die(std::size_t const w_div, std::size_t const h_div, double const proportion) {
-    constexpr std::size_t width{1024uz};
-    constexpr std::size_t height{width};
-    auto image{blank_image(width, height)};
-
-    sbx::CircleDrawer drawer{width, height};
-
-    auto const circles{drawer.draw_centred_rect_grid(w_div, h_div, proportion)};
-    for (auto const& c : circles) {
-        image.draw(c);
+        auto const circle{circle_drawer_.draw_centre(0.1)};
+        image.draw(circle);
+        image.write("circle.png");
     }
+    void draw_grid() {
+        auto image{blank_image()};
 
-    image.write(std::format("die_{}.png", w_div * h_div));
-}
-static void draw_x_die(std::size_t const back, std::size_t const fwd, double const proportion) {
-    constexpr std::size_t width{1024uz};
-    constexpr std::size_t height{width};
-    auto image{blank_image(width, height)};
-
-    sbx::CircleDrawer drawer{width, height};
-
-    auto draw_line{[&image, &drawer](std::size_t const n,
-                                     DiagonalLineDirection const direction,
-                                     double const proportion) -> void {
-        if (!n) {
-            return;
-        }
-
-        auto const circles{drawer.draw_centred_diagonal_line_grid(n, direction, proportion)};
+        auto const circles{circle_drawer_.draw_centred_rect_grid(2u, 2u, 0.05)};
         for (auto const& c : circles) {
             image.draw(c);
         }
-    }};
 
-    draw_line(fwd, DiagonalLineDirection::forward, proportion);
-    draw_line(back, DiagonalLineDirection::backward, proportion);
-
-    auto num{fwd + back};
-    if ((fwd % 2 != 0) && (back % 2 != 0)) {
-        // Middle dot overlaps
-        num--;
+        image.write("grid_image_0.png");
     }
 
-    image.write(std::format("die_{}.png", num));
-}
+    void draw_rect_die(std::size_t const w_div, std::size_t const h_div, double const proportion) {
+        auto image{blank_image()};
+
+        auto const circles{circle_drawer_.draw_centred_rect_grid(w_div, h_div, proportion)};
+        for (auto const& c : circles) {
+            image.draw(c);
+        }
+
+        image.write(std::format("die_{}.png", w_div * h_div));
+    }
+    void draw_x_die(std::size_t const back, std::size_t const fwd, double const proportion) {
+        auto image{blank_image()};
+
+        auto draw_line{[&](std::size_t const n,
+                           DiagonalLineDirection const direction,
+                           double const proportion) -> void {
+            if (!n) {
+                return;
+            }
+
+            auto const circles{
+                circle_drawer_.draw_centred_diagonal_line_grid(n, direction, proportion)};
+            for (auto const& c : circles) {
+                image.draw(c);
+            }
+        }};
+
+        draw_line(fwd, DiagonalLineDirection::forward, proportion);
+        draw_line(back, DiagonalLineDirection::backward, proportion);
+
+        auto num{fwd + back};
+        if ((fwd % 2 != 0) && (back % 2 != 0)) {
+            // Middle dot overlaps
+            num--;
+        }
+
+        image.write(std::format("die_{}.png", num));
+    }
+  private:
+    std::size_t width_{0u};
+    std::size_t height_{0u};
+    sbx::CircleDrawer circle_drawer_;
+};
+
 }
 
 int main(int /*argc*/, char** argv) {
@@ -98,17 +95,19 @@ int main(int /*argc*/, char** argv) {
     static_assert(sizeof(mgk::Quantum) == sizeof(float));
 
     using namespace sbx;
-    draw_circle();
-    draw_grid();
+    ImgGenerator ig{1024u, 1204u};
+
+    ig.draw_circle();
+    ig.draw_grid();
 
     constexpr double prop{0.05};
 
-    draw_rect_die(1, 1, prop);
-    draw_x_die(0u, 2u, prop);
-    draw_x_die(3u, 0u, prop);
-    draw_rect_die(2, 2, prop);
-    draw_x_die(3u, 3u, prop);
-    draw_rect_die(3, 2, prop);
+    ig.draw_rect_die(1, 1, prop);
+    ig.draw_x_die(0u, 2u, prop);
+    ig.draw_x_die(3u, 0u, prop);
+    ig.draw_rect_die(2, 2, prop);
+    ig.draw_x_die(3u, 3u, prop);
+    ig.draw_rect_die(3, 2, prop);
 
     return 0;
 }
