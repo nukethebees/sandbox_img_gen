23978e83447d89ae06a0505130eb758ece1efe7d
Matthew Love <matthewlove94@gmail.com>
2026-02-22 13:34:10 +0000

    Make simple main function

---
+++ sandbox_img_gen/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <Magick++.h>
+
+#include <print>
+
+int main(int /*argc*/, char** argv) {
+    Magick::MagickPlusPlusGenesis genesis{*argv};
+
+    std::print("Hello, world!");
+    return 0;
+}
--- sandbox_img_gen/sandbox_img_gen.h
+++
@@ -1,8 +0,0 @@
-﻿// sandbox_img_gen.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
