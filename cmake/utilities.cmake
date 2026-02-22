44d57131bdc25c631e7e7b0921d7f6060ab10d7f
Matthew Love <matthewlove94@gmail.com>
2026-02-22 14:33:19 +0000

    Fn to print strings

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -4,3 +4,7 @@ function(consume_setup_fn setup_fn)
     cmake_language(CALL ${setup_fn})
     unset(${setup_fn})
 endfunction()
+
+function(print_var var)
+    message(STATUS "${var}: ${${var}}")
+endfunction()
