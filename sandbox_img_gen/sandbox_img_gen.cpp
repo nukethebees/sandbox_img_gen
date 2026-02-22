58a72188df7b8924285343b1c16015242f6ff1a0
Matthew Love <matthewlove94@gmail.com>
2026-02-22 12:38:58 +0000

    Add project files.

---
+++ sandbox_img_gen/sandbox_img_gen.cpp
@@ -0,0 +1,12 @@
+﻿// sandbox_img_gen.cpp : Defines the entry point for the application.
+//
+
+#include "sandbox_img_gen.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
